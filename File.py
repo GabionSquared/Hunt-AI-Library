ad412e91ab62c1e6ae7cafcbf2e7fc7c52681413
Timothy White <Timothy.White5@outlook.com>
2022-06-12 17:54:34 +0100

    Detecting Similarity

--- File.py
+++ File.py
@@ -3,6 +3,34 @@ from PIL import Image
 import os
 PATH = os.path.dirname(__file__)
 
+def Similarity(im1, im2):
+    width1, height1 = im1.size
+    width2, height2 = im2.size
+    pix1 = im1.load()
+    pix2 = im2.load()
+
+    widthSmaller = width1
+    HeightSmaller = height1
+
+    if(width1 > width2):
+        widthSmaller = width2
+
+    if(height1 > height2):
+        HeightSmaller = height2
+
+    pixelsChecked = 0
+    pixelsSimilar = 0
+
+    for x in range(0, widthSmaller):
+        for y in range(0, HeightSmaller):
+
+            if(pix1[x,y] == pix2[x,y]):
+                pixelsSimilar += 1
+
+            pixelsChecked += 1
+
+    return ((pixelsSimilar/pixelsChecked)*100)
+
 #3 is wrong
 #2 is big, mid
 im = Image.open(PATH + '/Ref1.jpg')
@@ -19,7 +47,10 @@ InvenPanel = im.crop((offset_X, offset_Y, offset_X + 280, offset_Y + 555))
 sign  = InvenPanel.crop((8, 75, 8+72, 75+13))
 sign2 = InvenPanel.crop((8, 151, 8+72, 151+13))
 
-sign.save('sign.png')
-sign2.save('sign2.png')
-sign.show()
-sign2.show()
+small = Image.open(PATH + '/Lib/Small Sign.png')
+mid = Image.open(PATH + '/Lib/Medium Sign.png')
+large = Image.open(PATH + '/Lib/Large Sign.png')
+
+print("small: " + str(Similarity(sign, small)))
+print("mid: " + str(Similarity(sign, mid)))
+print("large: " + str(Similarity(sign, large)))
